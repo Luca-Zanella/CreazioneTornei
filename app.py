cdd0d6eb08b79943793d40c6d0611f543ec887b6
Luca <zanella.luca@iisgalvanimi.edu.it>
2021-08-13 11:32:48 +0200

    add pattern in base a quante squadre ci sono

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request
+from werkzeug.utils import redirect
+
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+
+    if request.method == "POST" and 'team' in request.form:
+        global numero_squadre
+        numero_squadre = request.form['team']
+
+        return redirect(url_for("league"))
+    else: 
+        return render_template("index.html")
+
+@app.route("/league")
+def league():
+    team_n = int(numero_squadre)
+    return render_template("league.html",team_n = team_n)
 
 if __name__ == '__main__':
     app.run(debug=True)
