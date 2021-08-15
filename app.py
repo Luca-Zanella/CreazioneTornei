688157e80cb7c36e8c80a5bc772513e43402fb2b
Luca <zanella.luca@iisgalvanimi.edu.it>
2021-08-15 10:38:29 +0200

    fix XMLHttprequest

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask,render_template,url_for,request
-from werkzeug.utils import redirect
-
+from logging import info
+from flask import Flask,render_template,url_for,request,redirect
+import random
+import itertools
+import numpy as np
+import json
 
 app = Flask(__name__)
 
@@ -15,14 +18,30 @@ def index():
     else: 
         return render_template("index.html")
 
-@app.route("/league")
+@app.route("/league",methods=["GET","POST"])
 def league():
     team_n = int(numero_squadre)
+    information = request.data.decode('utf-8')
+    #print(information)
+    #print(type(information))
 
+    if information != "":
+        information = json.loads(information)
+        #information = np.array(information)
+        set_size = 2
+        schedule = set()
+        teams = information
+        for comb in itertools.product(teams, repeat=set_size):
+            comb = sorted(list(comb))
+            if len(set(comb)) == set_size:
+                schedule.add(tuple(comb))
 
-    information = request.data
-    print(information)
-
+        schedule = list(schedule)
+        
+        random.shuffle(schedule)
+        schedule = np.asarray(schedule)
+        print(schedule)
+        print(type(schedule))
 
     return render_template("league.html",team_n = team_n)
 
